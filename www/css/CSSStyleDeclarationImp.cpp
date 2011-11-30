qbit/es-operating-system

commit 52cbbab93d655617b4cc124519c3a7fbe684cfb2

(CSSStyleDeclarationImp::processWhiteSpace) : Fix a bug.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2150 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/css/CSSStyleDeclarationImp.cpp
@@ -1363,8 +1363,6 @@ size_t CSSStyleDeclarationImp::processWhiteSpace(std::u16string& data, char16_t&
     case CSSWhiteSpaceValueImp::PreLine:
         for (int i = 0; i < data.length();) {
             char16_t c = data[i];
-            if (c == '\t')
-                data[i] = ' ';
             if (c == '\n') {  // linefeed
                 int j;
                 for (j = i - 1; 0 <= j && isSpace(data[j]); --j)
@@ -1385,7 +1383,11 @@ size_t CSSStyleDeclarationImp::processWhiteSpace(std::u16string& data, char16_t&
                     c = data[i] = ' ';
                     break;
                 }
-            } else if (c == ' ' && prevChar == ' ') {
+                if (i != 0)
+                    prevChar = 0;
+            } else if (c == '\t')
+                data[i] = ' ';
+            if (c == ' ' && prevChar == ' ') {
                 data.erase(i, 1);
                 continue;  // do not increment i.
             }

commit 13e90814d860907b368703decae68d53031d9c48

(CSSStyleDeclarationImp::processWhiteSpace) : Fix bugs introduced in r2150; cf. c5526c-display-000.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2180 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/css/CSSStyleDeclarationImp.cpp
@@ -1371,7 +1371,7 @@ size_t CSSStyleDeclarationImp::processWhiteSpace(std::u16string& data, char16_t&
                     data.erase(j, i - j);
                     i = j;
                 }
-                for (int j = i + 1; j < data.length() && isSpace(data[j]); ++j)
+                for (j = i + 1; j < data.length() && isSpace(data[j]); ++j)
                     ;
                 --j;
                 if (i < j)
@@ -1385,7 +1385,7 @@ size_t CSSStyleDeclarationImp::processWhiteSpace(std::u16string& data, char16_t&
                 if (i != 0)
                     prevChar = 0;
             } else if (c == '\t')
-                data[i] = ' ';
+                c = data[i] = ' ';
             if (c == ' ' && prevChar == ' ') {
                 data.erase(i, 1);
                 continue;  // do not increment i.