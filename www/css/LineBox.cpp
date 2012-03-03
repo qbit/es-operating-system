qbit/es-operating-system

commit 4b09ea31ce6aba639ce2f9745e90ed90bc02519f

(ViewCSSImp::layOutBlockBoxes, BlockLevelBox::layOutText, CSSStyleDeclarationImp::addBox, etc.) : Check and display margin, border, and padding at the left side and/or the right side of an empty inline elements; cf. block-in-inline-empty-001, block-in-inline-empty-003.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2464 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/css/LineBox.cpp
@@ -244,11 +244,17 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
     // the inline box to be generated must not be collapsed away by returning false.
     // cf. 10.8 Line height calculations: the ’line-height’ and ’vertical-align’ properties
     bool discardable = !data.empty();
+    if (discardable && style->display.isInline()) {
+        if (element.getFirstChild() == text && (style->marginLeft.getPx() || style->borderLeftWidth.getPx() || style->paddingLeft.getPx()) ||
+            element.getLastChild() == text && (style->marginRight.getPx() || style->borderRightWidth.getPx() || style->paddingRight.getPx()))
+            discardable = false;
+    }
 
-    if (style->processWhiteSpace(data, context->prevChar) == 0 && discardable) {
+    if (style->processWhiteSpace(data, context->prevChar) == 0) {
         if (!context->atLineHead && style->whiteSpace.isBreakingLines())
             context->breakable = true;
-        return !isAnonymous();
+        if (discardable)
+            return !isAnonymous();
     }
 
     bool psuedoChecked = isAnonymous() && getParentBox()->getFirstChild() != this;
@@ -263,9 +269,9 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
     InlineLevelBox* inlineBox = 0;
     InlineLevelBox* wrapBox = 0;    // characters moved to the next line
     for (;;) {
-        if (context->atLineHead && discardable && !wrapBox) {
+        if (context->atLineHead && !wrapBox) {
             size_t next = style->processLineHeadWhiteSpace(data, position);
-            if (position != next && data.length() <= next)
+            if (position != next && data.length() <= next && discardable)
                 return !isAnonymous();
             position = next;
         }
@@ -442,13 +448,13 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
             inlineBox->setData(font, point, inlineData, wrap - position, wrapWidth);
             inlineBox->width += advanced;
             position = next;
-        }
 
-        if (firstLetterStyle || data.length() <= position && isAtRightEdge(element, text))
-            inlineBox->width -= blankRight;
-        else {
-            inlineBox->clearBlankRight();
-            blankRight = 0;
+            if (firstLetterStyle || data.length() <= position && isAtRightEdge(element, text))
+                inlineBox->width -= blankRight;
+            else {
+                inlineBox->clearBlankRight();
+                blankRight = 0;
+            }
         }
 
         while (wrapBox) {
@@ -620,7 +626,7 @@ void LineBox::dump(std::string indent)
         child->dump(indent);
 }
 
-void InlineLevelBox::setData(FontTexture* font, float point, std::u16string data, size_t wrap, float wrapWidth)
+void InlineLevelBox::setData(FontTexture* font, float point, const std::u16string& data, size_t wrap, float wrapWidth)
 {
     assert(data[0] != 0 || data.empty());
     this->font = font;

www/css/Box.cpp
@@ -741,7 +741,6 @@ bool BlockLevelBox::layOutInline(ViewCSSImp* view, FormattingContext* context, f
                 collapsed = false;
             } else if (!isReplacedElement(element) && style->display.isInline()) {
                 // empty inline element
-                assert(!element.hasChildNodes());
                 if (layOutText(view, node, context, u"", element, style))
                     collapsed = false;
             } else {

www/css/Box.h
@@ -875,7 +875,7 @@ public:
 
     void resolveWidth();
     virtual void resolveOffset(ViewCSSImp* view);
-    void setData(FontTexture* font, float point, std::u16string data, size_t wrap, float wrapWidth);
+    void setData(FontTexture* font, float point, const std::u16string& data, size_t wrap, float wrapWidth);
     const std::u16string& getData() const {
         return data;
     }

www/css/CSSStyleDeclarationImp.cpp
@@ -1568,7 +1568,7 @@ void CSSStyleDeclarationImp::addBox(Box* box)
     else
         lastBox = box;
     // TODO: check the 3rd condition. maybe 'display'?
-    if (!parentStyle || parentStyle->isBlockLevel() || box->getBoxType() != Box::INLINE_LEVEL_BOX)
+    if (isBlockLevel() || !parentStyle || parentStyle->isBlockLevel() || box->getBoxType() != Box::INLINE_LEVEL_BOX)
         return;
     parentStyle->addBox(box);
 }

www/css/ViewCSSImp.cpp
@@ -259,10 +259,19 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Node node, BlockLevelBox* parentBox,
 
 BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Text text, BlockLevelBox* parentBox, CSSStyleDeclarationImp* style, CSSAutoNumberingValueImp::CounterContext* counterContext)
 {
+    bool discardable = true;
+    if (style->display.isInline()) {
+        Element element = interface_cast<Element>(text.getParentNode());
+        assert(element);
+        if (element.getFirstChild() == text && (style->marginLeft.getPx() || style->borderLeftWidth.getPx() || style->paddingLeft.getPx()) ||
+            element.getLastChild() == text && (style->marginRight.getPx() || style->borderRightWidth.getPx() || style->paddingRight.getPx()))
+            discardable = false;
+    }
+
     if (!parentBox || !style)
         return 0;
     if (!parentBox->hasChildBoxes()) {
-        if (!parentBox->hasInline() && style->whiteSpace.isCollapsingSpace()) {
+        if (discardable && !parentBox->hasInline() && style->whiteSpace.isCollapsingSpace()) {
             std::u16string data = text.getData();
             if (data.length() <= style->processLineHeadWhiteSpace(data, 0))
                 return 0;
@@ -275,7 +284,7 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Text text, BlockLevelBox* parentBox,
             table->processTableChild(text, style);
         return 0;
     }
-    if (!style->display.isInline() && !parentBox->hasAnonymousBox()) {
+    if (discardable && !style->display.isInline() && !parentBox->hasAnonymousBox()) {
         // cf. http://www.w3.org/TR/CSS2/visuren.html#anonymous
         // White space content that would subsequently be collapsed
         // away according to the 'white-space' property does not
@@ -447,7 +456,25 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Element element, BlockLevelBox* pare
     }
 
     if (!dynamic_cast<TableWrapperBox*>(currentBox)) {
-        bool emptyInline = style->display.isInline() && !element.hasChildNodes() && !isReplacedElement(element);
+        bool emptyInlineAtFirst = false;
+        bool emptyInlineAtLast = false;
+        if (style->display.isInline() && !isReplacedElement(element)) {
+            // Empty inline elements still have margins, padding, borders and a line height. cf. 10.8
+            if (!element.hasChildNodes())
+                emptyInlineAtFirst = true;
+            else {
+                if (style->marginLeft.getPx() || style->borderLeftWidth.getPx() || style->paddingLeft.getPx()) {
+                    Node child = element.getFirstChild();
+                    if (child.getNodeType() != Node::TEXT_NODE)
+                        emptyInlineAtFirst = true;
+                }
+                if (style->marginRight.getPx() || style->borderRightWidth.getPx() || style->paddingRight.getPx()) {
+                    Node child = element.getLastChild();
+                    if (child.getNodeType() != Node::TEXT_NODE)
+                        emptyInlineAtLast = true;
+                }
+            }
+        }
         BlockLevelBox* childBox = 0;
 
         CSSAutoNumberingValueImp::CounterContext ccPseudo(this);
@@ -476,7 +503,7 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Element element, BlockLevelBox* pare
                     counter->increment(1);
                 ccPseudo.update(markerStyle);
                 if (Element marker = markerStyle->content.eval(this, element, &cc)) {
-                    emptyInline = false;
+                    emptyInlineAtFirst = false;
                     map[marker] = markerStyle;
                     if (BlockLevelBox* box = layOutBlockBoxes(marker, currentBox, style, &cc))
                         childBox = box;
@@ -490,12 +517,19 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Element element, BlockLevelBox* pare
             if (!beforeStyle->content.isNone()) {
                 ccPseudo.update(beforeStyle);
                 if (Element before = beforeStyle->content.eval(this, element, &cc)) {
-                    emptyInline = false;
+                    emptyInlineAtFirst = false;
                     map[before] = beforeStyle;
                     if (BlockLevelBox* box = layOutBlockBoxes(before, currentBox, style, &cc))
                         childBox = box;
                 }
             }
+        }
+
+        if (emptyInlineAtFirst) {
+            if (!currentBox->hasChildBoxes())
+                currentBox->insertInline(element);
+            else if (BlockLevelBox* anonymousBox = currentBox->getAnonymousBox())
+                anonymousBox->insertInline(element);
         }
 
         TableWrapperBox* tableWrapperBox = 0;
@@ -510,23 +544,24 @@ BlockLevelBox* ViewCSSImp::layOutBlockBoxes(Element element, BlockLevelBox* pare
         if (tableWrapperBox && tableWrapperBox->isAnonymousTableObject())
             tableWrapperBox->layOutBlockBoxes();
 
+        if (emptyInlineAtLast) {
+            if (!currentBox->hasChildBoxes())
+                currentBox->insertInline(element);
+            else if (BlockLevelBox* anonymousBox = currentBox->getAnonymousBox())
+                anonymousBox->insertInline(element);
+        }
+
         CSSStyleDeclarationImp* afterStyle = style->getPseudoElementStyle(CSSPseudoElementSelector::After);
         if (afterStyle) {
             afterStyle->compute(this, style, element);
             if (!afterStyle->content.isNone()) {
                 ccPseudo.update(afterStyle);
                 if (Element after = afterStyle->content.eval(this, element, &cc)) {
-                    emptyInline = false;
                     map[after] = afterStyle;
                     if (BlockLevelBox* box = layOutBlockBoxes(after, currentBox, style, &cc))
                         childBox = box;
                 }
             }
-        }
-
-        if (emptyInline) {
-            // Empty inline elements still have margins, padding, borders and a line height. cf. 10.8
-            layOutBlockBoxes(Text(element.self()), currentBox, style, &cc);
         }
     }
 

commit 09d0aee691220cbbc1fd8868b09e7fd245cec901

(BlockLevelBox::layOutText) : Fix a bug introduced in r2464; cf. block-in-inline-whitespace-001a.
git-svn-id: http://es-operating-system.googlecode.com/svn/trunk@2468 d15c3b8f-5b43-0410-be93-4d5a55ce2954

www/css/LineBox.cpp
@@ -331,9 +331,13 @@ bool BlockLevelBox::layOutText(ViewCSSImp* view, Node text, FormattingContext* c
         bool linefeed = false;
         float advanced = 0.0f;
 
-        if (data.empty())
+        if (data.empty()) {
             inlineBox->setData(font, point, data, 0, 0);
-        else if (data[position] == '\n') {
+            if (!isAtRightEdge(element, text)) {
+                inlineBox->clearBlankRight();
+                blankRight = 0;
+            }
+        } else if (data[position] == '\n') {
             ++position;
             linefeed = true;
         } else {